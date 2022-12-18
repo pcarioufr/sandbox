073c368309ed53cfc47baf2087216b40fcaabfd0
pcarioufr <pcoid@pm.me>
2022-12-18 23:30:44 +0100

    remove terraform container after execution

--- terraform.sh
+++ terraform.sh
@@ -4,4 +4,4 @@ docker compose \
     --env-file sandbox.env \
     -f ./containers/compose.yml \
     -f ./containers/terraform/compose.yml \
-    run terraform -chdir=/config $@ 
+    run --rm terraform -chdir=/config $@ 
